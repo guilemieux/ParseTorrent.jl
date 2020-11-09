95f53499fad1227287762e1fcc3762b3af5041ec
Guillaume Lemieux <guilemieux@gmail.com>
2020-11-09 17:00:33 -0500

    add small test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,10 @@
 using Test
+using ParseTorrent: pieces2hashes, parsetorrent
 
+@testset "parsetorrent with a file" begin
+    @test parsetorrent(read("torrent-examples/bitlove-intro.torrent")) == Dict()
+end
+
+# @testset "pieces2hashes" begin
+#     @test pieces2hashes(Vector{UInt8}(1:100)) == []
+# end
